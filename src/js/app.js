e5d5a7d07121795ad17cfe153c0841d15f499973
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-01-31 16:58:37 +0100

    Add sort on click functionality for th elements

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,14 @@ let courses = [];
 
 window.onload = () => {
     fetchCourses();
+
+    /* add event listeners on th-headings to sort on click */
+    document.querySelector("#th-code").addEventListener("click", sortCode);
+    document.querySelector("#th-name").addEventListener("click", sortName);
+    document.querySelector("#th-progression").addEventListener("click", sortProgression);
 }
 
+/* function to fetch courses from an external url */
 async function fetchCourses() {
     const url = "https://webbutveckling.miun.se/files/ramschema_ht24.json";
     try {
@@ -20,6 +26,7 @@ async function fetchCourses() {
     }
 }
 
+/* function to display courses in table */
 function displayCourses(courses) {
     const tbodyEl = document.querySelector("tbody");
     tbodyEl.innerHTML = "";
@@ -28,8 +35,24 @@ function displayCourses(courses) {
         const rowEl = document.createElement("tr");
 
         rowEl.innerHTML = `<td>${course.code}</td><td>${course.coursename}</td><td>${course.progression}</td>`
-        
-
         tbodyEl.appendChild(rowEl);
     });
+}
+
+/* function to sort course code column */
+function sortCode() {
+    courses.sort((a, b) => a.code > b.code ? 1 : a.code < b.code ? -1 : 0);
+    displayCourses(courses);
+}
+
+/* function to sort course name column */
+function sortName() {
+    courses.sort((a, b) => a.coursename > b.coursename ? 1 : a.coursename < b.coursename ? -1 : 0);
+    displayCourses(courses);
+}
+
+/* function to sort progression column */
+function sortProgression() {
+    courses.sort((a, b) => a.progression > b.progression ? 1 : a.progression < b.progression ? -1 : 0);
+    displayCourses(courses);
 }
